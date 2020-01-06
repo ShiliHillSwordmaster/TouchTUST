0217f635ae2643c987ed4d184daed5d5b4f1f424
ShiliHillSwordmaster <47289189+ShiliHillSwordmaster@users.noreply.github.com>
2020-01-06 14:48:33 +0800

    Add files via upload

--- selectCourses/courses_scramble.py
+++ selectCourses/courses_scramble.py
@@ -8,28 +8,60 @@ import hashlib
 import re
 #import pytesseract
 
+#课程列表
+courses = {
+# "WL00170110_01",
+# "WL00170220_01",
+# "WL00170320_01",
+   # }
+# # "G900100110_101",
+#'WL00170420_01',
+'WL00170520_01',
+'WL00170610_01',
+'WL00170720_01',
+'WL00170810_01',
+'WL00170920_01',
+'WL00171010_01',
+'WL00171120_01',
+'WL00171210_01',
+'WL00171320_01',
+'WL00171420_01',
+'WL00171510_01',
+'WL00171610_01',
+'WL00171620_01',
+'WL00171710_01',
+'WL00171720_01',}
+# # "WL00171810_01" 
+# }
+
+lesson_code = "G900104310_101"  #课程编号
+term_code = "2019-2020-2-1"    #学期编号
+
+
 start_url = "http://jwxtxs.tust.edu.cn:46110/login"
 url = "http://jwxtxs.tust.edu.cn:46110/j_spring_security_check"#j_spring_security_check"
 captcha_url = "http://jwxtxs.tust.edu.cn:46110/img/captcha.jpg"
 callback_url = "http://jwxtxs.tust.edu.cn:46110/student/courseSelect/thisSemesterCurriculum/ajaxStudentSchedule/callback"
-#xuanke_url = "http://jwxtxs.tust.edu.cn:46110/student/courseSelect/selectCourses/waitingfor"
+waitingfor_url = "http://jwxtxs.tust.edu.cn:46110/student/courseSelect/selectCourses/waitingfor"
 xuanke_url = "http://jwxtxs.tust.edu.cn:46110/student/courseSelect/selectCourse/checkInputCodeAndSubmit"
 query_url = "http://jwxtxs.tust.edu.cn:46110/student/courseSelect/selectResult/query"
 token_url = "http://jwxtxs.tust.edu.cn:46110/student/courseSelect/courseSelect/index"
 clist_url = "http://jwxtxs.tust.edu.cn:46110/student/courseSelect/planCourse/courseList"
 queryTeacherJL_url = "http://jwxtxs.tust.edu.cn:46110/student/courseSelect/queryTeacherJL"
+getSectionAndTime_url = "http://jwxtxs.tust.edu.cn:46110/ajax/getSectionAndTime"
+courseSelect_url = "http://jwxtxs.tust.edu.cn:46110/student/courseSelect/courseSelect/index"
+planCourse_url = "http://jwxtxs.tust.edu.cn:46110/student/courseSelect/planCourse/index"
 
 login_datas = {
 "j_username": "",
 "j_password": "",
 "j_captcha": "",
 }
-lesson_code = "************"
-term_code = "2019-2020-2-1"
+
 
 clist = {
-"fajhh": "3742",
-"jhxn": "2019-2020-2-1",
+"fajhh": "",
+"jhxn": term_code,
 "kcsxdm": "",
 "kch": "",
 "kcm": "",
@@ -38,16 +70,30 @@ clist = {
 "jc": "0",
 }
 
+getSectionAndTime_datas = {
+'planNumber': '',
+'ff': 'j',
+}
+
 queryTeacherJL_datas = {
 "id": term_code+"_"+lesson_code,
 }
 
-xuanke_datas ={
-"dealType": "2",
-"fajhh": "3742",
+waitingfor_datas ={
+"dealType": "",
+"fajhh": "",
 "kcIds": lesson_code+"_"+term_code,
+#"kcms": "21019,36896,23398,95,49,48,49,",
+"sj": "",
+"kclbdm":"",
+}
 
-"sj": "0_0",
+xuanke_datas ={
+"dealType": "",
+"fajhh": "",
+"kcIds": lesson_code+"_"+term_code,
+#"kcms": "21019,36896,23398,95,49,48,49,",
+"sj": "",
 "kclbdm":"",
 "inputCode":"",
 "tokenValue":"",
@@ -63,6 +109,8 @@ captcha_headers = {
 "Referer": "http://jwxtxs.tust.edu.cn:46110/login",
 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
 }
+
+
 
 query_datas = {
 "kcNum": "",
@@ -87,6 +135,13 @@ def md5(str):
 def get_captcha_and_cookie(page):
 	try:
 		image = page.get(captcha_url,headers=captcha_headers)
+		if image.text == "":
+			print("captcha failed")
+			input()
+			return False
+		print("captcha: "+str(image.status_code))
+		
+		input()
 		f = open("captcha.png","wb")
 		f.write(image.content)
 		f.close()
@@ -94,7 +149,7 @@ def get_captcha_and_cookie(page):
 		#print(cookie)
 		#print("get")
 	except Exception as e:
-		print("try again")
+		print("try again"+str(e))
 		get_captcha_and_cookie(page)
 
 def DataEncoding(str):#预留加密解密
@@ -111,92 +166,8 @@ def test(html):#保留页面
 
 def query():
 	pass
-# try:
-	# image = requests.get(captcha_url,headers=captcha_headers)
-	# f = open(captcha.png,"wb")
-	# f.write(image)
-	# f.close()
-	# requests.post(start_url,datas=login_datas)
-# except:
-	# pass
-# image = requests.get(captcha_url,headers=captcha_headers)
-# f = open(r"C:\Users\Oberon\Desktop\captcha.png","wb")
-# f.write(image.content)
-# f.close()
-# requests.post(start_url)#,data=login_datas)
-# captcha = Image.open("C:\\Users\\Oberon\\Desktop\\captcha.png")
-# captcha.show()
+	
 
-# #captcha.show()
-
-# # colorlist = captcha.getcolors(1000000)
-# #print(colorlist)
-# #first = colorlist[0]
-# # for i,j in colorlist:
-	# # if i > first[0]:
-		# # second = first
-		# # first = colorlist[0]
-# pix = captcha.load()
-# size = captcha.size
-# captcha.filter(ImageFilter.BLUR)
-# #captcha.filter(ImageFilter.BLUR)
-# captcha.filter(ImageFilter.SHARPEN)
-# captcha.show()
-# ########################去黑线
-# for x in range(size[0]):
-	# for y in range(size[1]):
-		# pixal = pix[x,y]
-		# if (pixal[0]>100 and pixal[1] > 120 and pixal[2]>120) or (pixal[0]<150 and pixal[1] <170 and pixal[2] < 150):
-			# captcha.putpixel((x,y),(255,255,255))
-# captcha.show()
-# ########################去边框
-# for x in range(0,10):
-	# for y in range(0,10):
-		# captcha.putpixel((x,y),(255,255,255))
-# for x in range(size[0]-10,size[0]):
-	# for y in range(0,10):
-		# captcha.putpixel((x,y),(255,255,255))
-# for x in range(0,10):
-	# for y in range(size[1]-10,size[1]):
-		# captcha.putpixel((x,y),(255,255,255))
-# for x in range(size[0]-10,size[0]):
-	# for y in range(size[1]-10,size[1]):
-		# captcha.putpixel((x,y),(255,255,255))
-# #######################加粗补空
-# for x in range(size[0]):
-	# for y in range(size[1]):
-		# pixal = pix[x,y]
-		# if pixal[0] > 128 and pixal[1] < 100:
-			# captcha.putpixel((x,y),(0,0,0))
-		# else:
-			# try:
-				# pix_around = pix[x,y+1]
-				# try:
-					# pix_around = pix[x,y+1]
-					# true = 1
-				# except:
-					# pass
-				# if pix_around != (255,255,255) and true == 1:
-					# captcha.putpixel((x,y),(0,0,0))
-					
-			# except:
-				# pass
-			# try:
-				# pix_around = pix[x,y+2]
-				# if pix_around != (255,255,255):
-					# captcha.putpixel((x,y),(0,0,0))
-			# except:
-				# pass
-# #######################
-
-# #captcha.filter(ImageFilter.SMOOTH)
-# captcha.filter(ImageFilter.SHARPEN)
-# captcha.show()
-
-# result = pytesseract.image_to_string(captcha)
-# result = input("Input the captcha code")
-#captcha.show()
-#print("result = "+result)
 
 if __name__ == "__main__":
 	#reset()
@@ -205,22 +176,8 @@ if __name__ == "__main__":
 	schedule_data_check = 1
 	check_input = 1
 	
-	try:#读取课程表数据
-		ScheduleData =open("schedule.list","r")
-		data_str = ScheduleData.read()
-		ScheduleData.close()
-		JsonToLesson(data_str)
-		os.system("cls")
-		PrintClassList(ClassList)
-		input()
-		schedule_data_check = 0
-		print("read schedule , done")
-	except Exception as e:
-		#print(e)
-		pass
-	#print(schedule_data_check)
 
-	if schedule_data_check == 1:
+	if True:
 		try:#读取用户数据
 			user_data = open("database.data","r")
 			mima_length = DataDecoding(user_data.read(1))
@@ -253,18 +210,24 @@ if __name__ == "__main__":
 				login_datas["j_username"] = xuehao
 				login_datas["j_password"] = md5(mima)
 				
-			get_captcha_and_cookie(page)
+			#获取验证码图片
+			if get_captcha_and_cookie(page) == False:
+				print("error")
+				exit()
 			captcha = Image.open("captcha.png")
-			captcha.show()
+			captcha.show()#显示图片
+			
 			#cr.WatchCaptcha(captcha)
-			print("please read the captcha:")
+			print("please read the captcha:") #读取验证码
 			result = input() 
 			login_datas['j_captcha'] = result
 			
 			#os.remove('captcah.png')
-			p = page.post(url,data = login_datas)#提交表单
+			p = page.post(url,data = login_datas)#提交登录表单
 			#input(p.headers)
-			if (p.url == "http://jwxtxs.tust.edu.cn:46110/login?errorCode=badCredentials"):#判断是否登录
+			
+			#判断是否登录
+			if (p.url == "http://jwxtxs.tust.edu.cn:46110/login?errorCode=badCredentials"):
 				print("try again")
 				check_input = 0
 			elif (p.url == "http://jwxtxs.tust.edu.cn:46110/login?errorCode=badCaptcha"):	
@@ -278,49 +241,97 @@ if __name__ == "__main__":
 				break
 			else:
 				check_input = 0
-				
+			#登录结束
+			
 		input("xuanke?")
 		input()
-		#获取选课列表
+		#
 		
 		#if user_data_check == 1:
 		 #开始选课
 		try:
+			#获得方案计划号
+			p = page.get(courseSelect_url)
+			fajhh = re.search("fajhh=.*\'",p.text)
+			fajhh = fajhh.group()
+			# print(fajhh)
+			fajhh = fajhh[-5:-1]
+			print('fajhh:'+fajhh)
+			p = page.get(planCourse_url+"?fajhh="+fajhh,data = {"fajhh":fajhh,}) #get fajhh
+			print("planCourse_index: "+str(p.status_code))
+			# print(p.text)
+			dealType = re.search("dealType\" value=\".*\"",p.text)
+			dealType = dealType.group()
+			dealType = dealType[-2:-1]
+			print('dealType:'+dealType)
+			
+			sj = re.search("sj\" value=\".*\"",p.text)
+			sj = sj.group()
+			sj = sj[-4:-1]
+			print('sj:'+sj)
+			
+			
 			p = page.get(token_url) #获得token
+			#print(p.headers)
 			#print(p.text)
 			mj = re.search(r"id=\"tokenValue\" value=\".*\"",p.text)
 			mjstr = mj.group()
+			print("mjstr: "+mjstr)
 			tokenValue = mjstr[23:-1]
+			print("tokenValue: "+tokenValue)
+			
+			
+			#准备选课表单内容
 			xuanke_datas["tokenValue"]= tokenValue
-			#print(tokenValue)
-			# while True:
-				# try:
-					# mj = re.search(input("pattern:   :"),p.text)
-					# print(mj.group())
-				# except Exception as e:
-					# print(e)
-			courses = {""}
-		#提交学科表单
-			courses = {          #此处填入课程号
-'','','',				
-
-}
+			xuanke_datas["fajhh"] = fajhh
+			xuanke_datas['dealType'] = dealType
+			xuanke_datas['sj'] = sj
+			waitingfor_datas["fajhh"] = fajhh
+			waitingfor_datas['dealType'] = dealType
+			waitingfor_datas['sj'] = sj
+			
+			
+			#p = page.post(getSectionAndTime_url,data =getSectionAndTime_datas )
+			#print(p.url)
+			#print(p.headers)
+					
+			
+		
 			for i in courses:
 				lesson_code = i
-				p = page.post(queryTeacherJL_url,data = queryTeacherJL_datas)
-				print(p.status_code)
-				p = page.post(xuanke_url,data = xuanke_datas)
-				print(p.url)
-				print(p.status_code)
+				queryTeacherJL_datas["id"] = term_code+"_"+lesson_code
+				
+				#模拟勾选
+				p = page.post(queryTeacherJL_url,data = queryTeacherJL_datas) 
+				print("queryTeacherJL:" + str(p.status_code))
+				#print("queryTeacherJL_datas: "+str(queryTeacherJL_datas))
+				
+				kcIds = lesson_code+"_"+term_code
+				xuanke_datas["kcIds"] = kcIds
+				print(kcIds)
+				
+				searchtj = lesson_code[0:10]
+				xuanke_datas["searchtj"] = searchtj
+				print(searchtj)
+				
+				#提交选课表单
+				p = page.post(xuanke_url,data = str(xuanke_datas))
+				#print("xuanke_datas: "+str(xuanke_datas))
+				print("xuanke:" + str(p.status_code))
 				#print(p.text)
 				bb = json.loads(p.text)
-				print(bb)
+				print("response_ "+str(bb))
 				
-				query_datas["redisKey"] = xuehao+"2"
+				query_datas["redisKey"] = xuehao+dealType
 				query_datas["kcNum"] = '1'
-				print(query_datas)
+				#print("query_datas: "+str(query_datas))
 				
-
+				#提交表单等待结果
+				p = page.post(waitingfor_url,data = waitingfor_datas)
+				#print("waitingfor_datas: "+str(waitingfor_datas))
+				print("waiting ..."+str(p.status_code))
+				
+				
 				while True:
 					#查询选课结果
 					pp = page.post(query_url,data = query_datas)
